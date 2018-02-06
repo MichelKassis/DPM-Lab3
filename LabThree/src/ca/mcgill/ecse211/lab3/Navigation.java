0f8c43984499c6ce38d13dba517ad616b0310960
Michel Kassis <michelkassis@Michels-MacBook-Pro-2.local>
2018-02-05 22:48:41 -0500

    kevin code

--- LabThree/src/ca/mcgill/ecse211/lab3/Navigation.java
+++ LabThree/src/ca/mcgill/ecse211/lab3/Navigation.java
@@ -3,44 +3,28 @@ package ca.mcgill.ecse211.lab3;
 
 
 import ca.mcgill.ecse211.sensor.Odometer;
-
-
+import lejos.hardware.Sound;
 import lejos.hardware.ev3.LocalEV3;
-
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.motor.EV3MediumRegulatedMotor;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.robotics.SampleProvider;
+
 
 public class Navigation extends Thread{
 	//declare the port occupied by the motor and sensor
-		public static final EV3LargeRegulatedMotor leftMotor =
-				new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
-		public static final EV3LargeRegulatedMotor rightMotor =
-				new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
-//		public static final EV3UltrasonicSensor ultraSensor =
-//				new EV3UltrasonicSensor(LocalEV3.get().getPort("S1"));
-//		public static final EV3MediumRegulatedMotor spinMotor= 
-//				new EV3MediumRegulatedMotor(LocalEV3.get().getPort("S3"));
-	
-	
-	
-	
+	public static final EV3LargeRegulatedMotor leftMotor  =	new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+	public static final EV3LargeRegulatedMotor rightMotor =	new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+
 	private static Odometer odo;
-	private static SampleProvider us;
-	private static float[] usData;
 	
 	public final double tileSize= 30.48;
+	private static boolean isNavigating;
 	private static int x[];
 	private static int y[];
-
-
-
-
+	private boolean notSmallValue = true;
+	
 
 	public Navigation(int X[], int Y[]) {
-		x=X;
-		y=Y;
+		this.x = X;
+		this.y = Y;
 
 	}
 
@@ -48,119 +32,154 @@ public class Navigation extends Thread{
 		odo = lab3.odometer;
 		//loop all the points
 		for (int i = 0; i < x.length; i++) {
-			travelto(x[i]*tileSize, y[i]*tileSize);
-
+			travelTo(x[i]*tileSize, y[i]*tileSize);
 		}
 		//end program
 		System.exit(0);
 
 	}
 
-	public void travelto(double i, double j) {
+	public void travelTo(double i, double j) {
 
+		isNavigating = true;
 		//initialize
 		double direction = 0;
 		//find direction and distance in cm
-		double deltaX = i-odo.getX();
-		double deltaY = j-odo.getY();	
+		double deltaX = i - odo.getX();
+		double deltaY = j - odo.getY();	
 		double distance = Math.sqrt((deltaX*deltaX)+(deltaY*deltaY));
-	
 		
+		System.out.println("X:"+deltaX);
+		System.out.println("Y:"+deltaY);
+
 		//in case deltaY goes to 0 though it has really small possibility to be actually 0
 		try{
 			direction = Math.toDegrees(Math.atan(deltaX/deltaY));
 		}
 		catch(ArithmeticException e ){
-			
+
 		}
-		
+
 		//filter out small error that cause deltaX/deltaY to be negative
-		if(deltaY<5&&deltaY>-5){
-			if (deltaY<0) {
-				direction = 180;
-			}
-			else{
-				direction = 0;
-			}
-			
-		}
-		if(deltaY<5&&deltaY>-5){
+		if( deltaY <3 && deltaY > -3){
 			if (deltaX<0) {
 				direction = -90;
 			}
 			else{
 				direction = 90;
 			}	
-		}
-		
-		// if delta X and delta Y both <0, direction+180
-		if (!(deltaY<5&&deltaY>-5) && !(deltaY<5&&deltaY>-5) && deltaX<0&&deltaY<0) {
-			direction +=180;
+			System.out.println("b");
+			notSmallValue = false;
+			
 		}
 
+		if(deltaX<3&&deltaX>-3){
+			if (deltaY < 0) {
+				direction = 180;
+			}
+			else{
+				direction = 0;
+			}
+			System.out.println("a");
+		notSmallValue = false;
+		}
+
+		System.out.println("smallv:"+notSmallValue);
+		// if delta X and delta Y both <0, direction+180
+		if (notSmallValue && deltaX > 0 && deltaY < 0) {
+			direction +=180;
+			System.out.println("c");
+			
+		}
+
+		System.out.println(direction);
+			
 		turnTo(direction);
-		
+
 		// move forward with the distance calculated
 		leftMotor.rotate(convertDistance(lab3.WHEEL_RAD, distance), true);
 		rightMotor.rotate(convertDistance(lab3.WHEEL_RAD, distance), false);
-		 
+		isNavigating = false;
+		notSmallValue = true;
+	}
 	
 
-	}
-
 	public static void turnTo(double theta) {
-		
+
+		isNavigating = true;
+
+		//find the direction
 		double turn = theta - odo.getTheta();
-		if(turn>180){
+		if(turn > 180){
 			turn -= 360;
 		}
-		else if(turn<-180){
+		else if(turn < -180){
 			turn +=360;
 		}
-		leftMotor.setSpeed(lab3.ROTATE_SPEED);
-		rightMotor.setSpeed(lab3.ROTATE_SPEED);
 		
 		turn %=360;
+		
+		// set rotate speed
+		leftMotor.setSpeed(lab3.ROTATE_SPEED);
+		rightMotor.setSpeed(lab3.ROTATE_SPEED);
+
+		
+		//decide which way to turn
 		if (turn>0) {
 			leftMotor.rotate(convertAngle(lab3.WHEEL_RAD, lab3.TRACK, turn), true);
 			rightMotor.rotate(-convertAngle(lab3.WHEEL_RAD, lab3.TRACK, turn), false);
-			
+
 		}
+		
 		//turn the other way
 		else {
 			turn = -turn;
 			leftMotor.rotate(-convertAngle(lab3.WHEEL_RAD, lab3.TRACK, turn), true);
 			rightMotor.rotate(convertAngle(lab3.WHEEL_RAD, lab3.TRACK, turn), false);	
 		}
+
 	}
 
 	public static void avoid(){
-		
+		// 23, 14
 		//get data from the ultrasonic sensor
-		us.fetchSample(usData,0);
-		float obDist = usData[0];
-		
-		// if our EV3 is too close to the obstacle
-		if(obDist<50){
-			turnTo(odo.getTheta()+90);
-			leftMotor.rotate(convertDistance(lab3.WHEEL_RAD, 20), true);
-			rightMotor.rotate(convertDistance(lab3.WHEEL_RAD, 20), false);
+		leftMotor.stop();
+		rightMotor.stop();
+		leftMotor.setSpeed(lab3.ROTATE_SPEED);
+		rightMotor.setSpeed(lab3.ROTATE_SPEED);
+		leftMotor.rotate(convertAngle(lab3.WHEEL_RAD, lab3.TRACK,90), true);
+		rightMotor.rotate(-convertAngle(lab3.WHEEL_RAD, lab3.TRACK, 90), false);
+		double currentX = odo.getX();
+		double currentY = odo.getY();
+		while(currentX+20>odo.getX() &&currentY+20>odo.getY()) {
+			leftMotor.forward();
+			rightMotor.forward();
 		}
-		
-	}
-	
-	
-	
-	
+		leftMotor.stop();
+		rightMotor.stop();
+		leftMotor.rotate(-convertAngle(lab3.WHEEL_RAD, lab3.TRACK,75), true);
+		rightMotor.rotate(convertAngle(lab3.WHEEL_RAD, lab3.TRACK, 75), false);
+		currentX=odo.getX();
+		currentY=odo.getY();
+		while(currentX+20 > odo.getX() && currentY+20 > odo.getY()) {
+			leftMotor.forward();
+			rightMotor.forward();
+		}
 
+		isNavigating = false;
+	}
+
+	public boolean isNavigating(){
+		return isNavigating;
+	}
 
 	//copied from code given in last lab
-	  public static int convertDistance(double radius, double distance) {
-		    return (int) ((180.0 * distance) / (Math.PI * radius));
-		  }
+	public static int convertDistance(double radius, double distance) {
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
 
-		  public static int convertAngle(double radius, double width, double angle) {
-		    return convertDistance(radius, Math.PI * width * angle / 360.0);
-		  }
+	public static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
 
 }
--- LabThree/src/ca/mcgill/ecse211/lab3/lab3.java
+++ LabThree/src/ca/mcgill/ecse211/lab3/lab3.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse211.lab3;
 
+
+
+
+
+
 import ca.mcgill.ecse211.sensor.Odometer;
 import ca.mcgill.ecse211.sensor.OdometerExceptions;
 import lejos.hardware.Button;
@@ -12,10 +17,12 @@ public class lab3 {
 	
 
 	//basic info about my EV3
-	public static final double WHEEL_RAD = 2.0;  
-	public static final double TRACK = 11.0; 
+	public static final double WHEEL_RAD = 2.16;  
+	public static final double TRACK = 11.7; 
 	public static final int FORWARD_SPEED = 150;
 	public static final int ROTATE_SPEED = 150;
+	public static final int LOW_SPEED = 100;
+	public static final int errorRange = 150;
 	public static float OBSTACLEDIST = 100;
 
 	
@@ -28,11 +35,18 @@ public class lab3 {
 	//maps
 	
 	
-//	private static int xarr[] = {1,2,2,0,1};                                   
-//	private static int yarr[] = {0,1,2,2,1};
+	private static int map1x[] = {0,1,2,2,1};                                   
+	private static int map1y[] = {2,1,2,1,0};
 
 	private static int map2x[] = {1,0,2,2,1};
 	private static int map2y[] = {1,2,2,1,0};
+	
+	private static int map3x[] = {1,2,2,0,1};
+	private static int map3y[] = {0,1,2,2,1};
+	
+	private static int map4x[] = {0,1,1,2,2};
+	private static int map4y[] = {1,2,0,1,2};
+			
 
 
 	public static void main(String[] args) throws OdometerExceptions {
@@ -42,19 +56,19 @@ public class lab3 {
 
 		// Odometer related objects
 		odometer = Odometer.getOdometer(Navigation.leftMotor, Navigation.rightMotor, TRACK, WHEEL_RAD); 
-		Navigation navigator = new Navigation(map2x,map2y);
+		Navigation navigator = new Navigation(map3x,map3y);
 		
 		 
 		// implementation			
 		Thread odoThread = new Thread(odometer);
-		//Thread Avoidance = new Thread();
+		Thread Avoidance = new Thread();
 		Thread Navigator = new Thread(navigator);
 		odoThread.setPriority(3);
-		//Avoidance.setPriority(2);
+		Avoidance.setPriority(2);
 		Navigator.setPriority(1);
 		
 		odoThread.start();
-		//Avoidance.start();
+		Avoidance.start();
 		
 		
 
